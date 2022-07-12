ed78e568644648f225ef38c6e26c88b8a622ece7
ed00m:th3d00m <pabloze@gmail.com>
2022-07-12 05:25:57 -0400

    [adv] - Se agrega esqueleto para servicio de indicadores.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndicadoresComponent } from './service/indicadores/indicadores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndicadoresComponent
   ],
   imports: [
     BrowserModule,
